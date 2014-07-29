8e387f894171870ee6e0b64b900dd1cfaed1d013
Sophal Kim <sophal.kim.work@gmail.com>
2014-07-28 21:00:51 -0700

    Bolded all Attributes with addClass()

--- scripts/jquery_practice.js
+++ scripts/jquery_practice.js
@@ -6,3 +6,12 @@
 $(document).ready(function() {
 	alert("welcome");
 });
+
+$(document).ready(function() {
+	$("a").click(function(event) {
+		alert("The link will no longer take you to jquery.com");
+		event.preventDefault();
+	});
+});
+
+$("a").addClass("test");
