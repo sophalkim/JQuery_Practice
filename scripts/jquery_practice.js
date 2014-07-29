8c1e379a9259cd0ad9f9ee2afa97a0ab19ddaad7
Sophal Kim <sophal.kim.work@gmail.com>
2014-07-28 21:09:26 -0700

    Animation effects

--- scripts/jquery_practice.js
+++ scripts/jquery_practice.js
@@ -7,11 +7,16 @@ $(document).ready(function() {
 	alert("welcome");
 });
 
-$(document).ready(function() {
-	$("a").click(function(event) {
-		alert("The link will no longer take you to jquery.com");
-		event.preventDefault();
-	});
-});
+// $(document).ready(function() {
+// 	$("a").click(function(event) {
+// 		alert("The link will no longer take you to jquery.com");
+// 		event.preventDefault();
+// 	});
+// });
 
 $("a").addClass("test");
+
+$("a").click(function() {
+	event.preventDefault();
+	$(this).hide("slow");
+});
